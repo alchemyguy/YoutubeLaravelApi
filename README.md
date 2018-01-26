e80f26916bd2f124aafcdd7bfb013913ae08daa2
Mukesh Chandra <mukesh201722@gmail.com>
2018-01-26 16:42:26 +0530

    Initial SetUp

---
+++ README.md
@@ -0,0 +1,2 @@
+### Youtube Laravel API
+=================
---
+++ src/YoutubeLaravelApiServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AlchemyGuy\YoutubeLaravelApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class YoutubeLaravelApiServiceProvider extends ServiceProvider {
+	/**
+	 * Perform post-registration booting of services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+	}
+
+	/**
+	 * Register any package services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
+}
